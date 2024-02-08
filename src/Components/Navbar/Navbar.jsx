8ee9901e37d0692c8286f769d831bf0bf7b06358
Fpelourson <fede_pelourson@hotmail.com>
2024-02-08 15:09:54 -0300

    as

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -15,13 +15,13 @@ const Navbar = () => {
         </motion.span>
         <div className="social">
           <a href="https://github.com/Fpelourson" target="blank">
-            <img src="./assets/git.png"></img>
+            <img src="./public/git.png"></img>
           </a>
           <a href="https://www.linkedin.com/in/federico-pelourson/" target="blank">
-            <img src="./assets/linkedin.png"></img>
+            <img src="./public/linkedin.png"></img>
           </a>
           <a href="https://www.instagram.com/fedepelourson/" target="blank">
-            <img src="./assets/instagram.jpg"></img>
+            <img src="./public/instagram.jpg"></img>
           </a>
         </div>
       </div>
